a8a312dc81ee0799ffef12b9a39172de363f0a3e
Brayden Price <brayden@braydenprice.dev>
2023-05-21 15:05:27 -0400

    Inital Commit

---
+++ app.vue
@@ -0,0 +1,18 @@
+<script setup>
+import NavBarVue from "./components/navbar.vue";
+</script>
+
+<template>
+  <div>
+    <NavBarVue></NavBarVue>
+    <div>
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </div>
+  </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&family=DM+Serif+Display:ital@0;1&family=DM+Serif+Text:ital@0;1&display=swap');
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script>
+</script>
+
+<template>
+    <div class="flex items-center justify-center h-52">
+    </div>
+</template>
+
+<style></style>
